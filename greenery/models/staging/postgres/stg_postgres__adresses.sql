8b7e88956d00fdaa5ab14abd7c39d7611fbcf484
Nico Steffen <nico.steffen@neuefische.de>
2023-04-17 17:28:32 +0200

    improved readability for sql

--- greenery/models/staging/postgres/stg_postgres__adresses.sql
+++ greenery/models/staging/postgres/stg_postgres__adresses.sql
@@ -4,10 +4,19 @@
   )
 }}
 
-SELECT
-    address_id as address_guid,
-    address,
-    lpad(zipcode,5,0) as zipcode,
-    state,
-    country
-FROM {{ source('postgres', 'addresses') }}
+with 
+source as (
+	SELECT *
+		FROM {{ source('postgres','adresses') }}
+	),
+renamed_recast as (
+
+	SELECT
+    	address_id as address_guid,
+	address,
+	lpad(zipcode,5,0) as zipcode,
+	state,
+    	country
+FROM source
+
+select * from renamed_recast
